0039fd33539afa028618440af677b1334db32c15
Sándor Martin Tibor <martinsandor707@gmail.com>
2023-05-02 00:07:31 +0200

    Adjusted checkstyle plugin and wrote first javadocs

--- src/main/java/puzzlegame/PuzzleGameModel.java
+++ src/main/java/puzzlegame/PuzzleGameModel.java
@@ -4,7 +4,8 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 
 /**
- * The model of the main game
+ * The model of the main game.<br>
+ * Contains methods for accessing the individual cells in the GridPane after initialization
  */
 public class PuzzleGameModel {
         public static final int BOARD_SIZE=10;
@@ -13,10 +14,10 @@ public class PuzzleGameModel {
         private final ReadOnlyObjectWrapper<Cell>[][] board = new ReadOnlyObjectWrapper[2][BOARD_SIZE];
 
         /**
-         * Initializes the {@<code>ReadOnlyObjectWrapper board</code>} board, storing if a given cell is valid or not
+         * Initializes the {@link #board}, storing if a given cell is valid or not.<br>
          * It currently sets up the board according to the picture provided in the project's README.md, but the variables
-         * {@<code>BOARD_SIZE</code>} and {@<code>EXTRA_CELL_INTERVAL</code>} give some extra customizability.
-         * In the future it would probably be best to get the value of {@<code>BOARD_SIZE</code>} directly from the GridPane Object
+         * {@link #BOARD_SIZE} and {@link #EXTRA_CELL_INTERVAL} give some extra customizability.
+         * In the future it would probably be best to get the value of {@link #BOARD_SIZE} directly from the GridPane Object
          */
         public PuzzleGameModel() {
                 for (int i=0; i<BOARD_SIZE; i++){       //The first row of the GridPane is mostly invalid
@@ -28,7 +29,15 @@ public class PuzzleGameModel {
                         board[1][i]=new ReadOnlyObjectWrapper<Cell>(Cell.VALID);
                 }
         }
-        public ReadOnlyObjectProperty<Cell> squareProperty(int i, int j) {
+
+        /**
+         * Returns whether the specified cell is valid or not
+         * @param i The row of the given cell
+         * @param j The column of the given cell
+         * @return The enum of the {@link Cell}
+         */
+        public ReadOnlyObjectProperty<Cell> CellProperty(int i, int j) {
                 return board[i][j].getReadOnlyProperty();
         }
+
 }
---
+++ src/main/java/puzzlegame/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This is the package of the main game.
+ * It contains all the files related to <a href="../../resources/game.fxml">game.fxml</a>.
+ */
+
+package puzzlegame;
